63abbf8288dddb8139710418a0a25c423df7c291
GTP <50524677+GaThsaPer@users.noreply.github.com>
2025-06-03 22:13:59 +0200

    Delete build directory

--- build/external/libxl/CMakeFiles/copy_libxl_libs.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/copy_libxl_libs"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/copy_libxl_libs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
